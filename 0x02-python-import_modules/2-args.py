8beff0ace348b0985d2d3bd1b28aa1fcd96abc63
Elnatan999 <krosection999@gmail.com>
2021-09-09 19:01:19 +0000

    5th commit trail 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,7 +2,7 @@
 def main():
    import sys
    i = len(sys.argv)
-   if i - 1 > 0:
+   if i - 1 == 1:
       print("{0}".format(i - 1),"argument:")
    elif i - 1 > 1:
       print("{0}".format(i - 1),"arguments:")
