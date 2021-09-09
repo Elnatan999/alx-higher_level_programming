39fe5de8227214bb2078cad28591f78f7f6b7fb0
Elnatan999 <krosection999@gmail.com>
2021-09-09 13:49:44 +0000

    4th python commit 3rd trail

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -3,6 +3,6 @@ def main():
    from add_0 import add
    a = 1
    b = 2
-   print(a, "+", b, "=" ,add(1, 2))
+   print("{0}".format(a), "+", "{0}".format(b), "=" ,add(a, b))
 if __name__ == '__main__':
    main()
