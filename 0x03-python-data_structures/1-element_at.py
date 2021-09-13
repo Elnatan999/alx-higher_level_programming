b9d51a526c589174f706431f68a3dc0f205da58b
Elnatan999 <krosection999@gmail.com>
2021-09-13 13:33:45 +0000

    task 3 commit all

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-   if idx  >= 0:
-      if idx < len(my_list):
-         return (my_list[idx])
-      else:
-         return ("None")
-   else:
-      return ("None")
+    if idx >= 0:
+        if idx < len(my_list):
+            return (my_list[idx])
+        else:
+            return ("None")
+    else:
+        return ("None")
