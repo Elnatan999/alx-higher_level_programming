f70009d06412caab4f90ecd260c3c4a327b43e2f
Elnatan999 <krosection999@gmail.com>
2021-09-21 13:02:35 +0000

    commit 2

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 for i in range(00, 100):
     if i < 99:
-        print("%02d"%i,",", end = "")
+        print("%02d,"%i,"", end = "")
     elif i > 98:
         print(i)
     else:
--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -2,8 +2,9 @@
 def uppercase(str):
     str2 = ""
     for i in str:
-        if ord(i) >= 97 and ord(i) <= 122:
+        if ord(i) >= 97 and ord(i) < 123:
             str2 += chr(ord(i) - 32)
         else:
             str2 += i
     print(str2)
+        
