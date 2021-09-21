1b4bf87c855e107b4642ffb5867d38ba34008b87
Elnatan999 <krosection999@gmail.com>
2021-09-21 12:07:24 +0000

    python long time no see

---
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -0,0 +1,3 @@
+#!/usr/bin/python3
+for i in range(0, 99):
+    print("{0}".format(i), "=", "{0}".format(hex(i)))
---
+++ 0x01-python-if_else_loops_functions/tibebe.py
@@ -0,0 +1,3 @@
+#!/usr/bin/python3
+string_value = \u1267
+print("{0}".format(string_value))
